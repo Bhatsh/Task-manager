4fd103a22737e51722b00aba8c6c283f5daa63b5
Affan Hakak <affanshowkat@gmail.com>
2022-08-18 14:35:10 +0530

    added tasks and cdk property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, TodoComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    DragDropModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { ITask } from '../model/task';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  taskForm!: FormGroup;
 
-  constructor() { }
+  tasks: ITask[] = [];
+  inProgress: ITask[] = [];
+  done: ITask[] = [];
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.taskForm = this.fb.group({
+      item: ['', Validators.required],
+    });
+  }
+  addTask() {
+    this.tasks.push({
+      description: this.taskForm.value.item,
+      done: false,
+    });
+  }
+  deleteTask(i: number) {
+    this.tasks.splice(i, 1);
+  }
+  deleteInProgressTask(i: number) {
+    this.inProgress.splice(i, 1);
+  }
+  editTask(i: number) {
+    console.log('oii');
   }
 
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
